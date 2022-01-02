59dd1e6ef3bdf758270cf526efe45c6d1d3cc1d9
Fahira <fahiratasla13@gmail.com>
2022-01-03 00:11:25 +0700

    first commit

---
+++ app/src/main/java/com/example/behealthy/PolaMakan.java
@@ -0,0 +1,14 @@
+package com.example.behealthy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PolaMakan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pola_makan);
+    }
+}
